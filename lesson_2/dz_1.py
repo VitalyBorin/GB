936a990d915fcb8f1ccf0a11dcc687c9d52f8f7f
VitalyBorin <vitaly.borin@gmail.com>
2021-01-12 02:03:48 +0300

    dz_1

--- lesson_2/dz_1.py
+++ lesson_2/dz_1.py
@@ -1,1 +1,7 @@
-a = 0
+a = [True, 1, 'привет']
+b = 0
+dict = {'bool': 'булевая', 'int': 'целочисленная', 'str': 'строковая'}
+for el in a:
+    print(el, end='')
+    print(' - '+dict.get(str(type(a[b])).split("'")[1]))
+    b += 1
