baaff29b9b7518e0cbdfff2b79b8732037b2c9b1
VitalyBorin <vitaly.borin@gmail.com>
2021-01-17 01:36:50 +0300

    lesson 4

--- lesson_4/dz_4.py
+++ lesson_4/dz_4.py
@@ -1,11 +1,17 @@
 a = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 b = []
-# b = [el1 for el1 in a for el2 in a if el1 == el2]
-for el in a:
-    for el2 in a:
-
-    b.append()
-
-
 print(a)
+flag = False
+
+# b = [el for el in a for el2 in b if el != el2]
+# не придумал как сделать в одну строку. Почему-то не работает
+
+for el in a:
+    for el2 in b:
+        if el == el2:
+            flag = True
+    if not flag:
+        b.append(el)
+    flag = False
+
 print(b)
